10d1b2546df6251b0a1e3f1cd0e2f557ea444c34
Matthew Thelim <matthewthelim@gmail.com>
2026-05-19 16:54:41 +0800

    chore: remove unused field import from schemas

--- src/data/schemas.py
+++ src/data/schemas.py
@@ -6,7 +6,7 @@ No component defines its own versions of these types.
 """
 
 from __future__ import annotations
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from datetime import datetime
 from enum import Enum
 from typing import Optional
